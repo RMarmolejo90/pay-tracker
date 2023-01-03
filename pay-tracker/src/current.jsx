d970efb72ef2f603ba7166f04ce796d4d43ba2d8
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-01-03 07:50:44 -0500

    started template for react app

---
+++ pay-tracker/src/current.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Current (props){
+    return(
+        <div>
+            <CurrentTimer/>
+            <CurrentPay/>
+        </div>
+    )
+}
+
+export default Current;
---
+++ pay-tracker/src/hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Hero(){
+    return(
+        <div>
+            <button className="settings">
+                Settings
+            </button>
+            <h1>
+                Pay-Checker
+            </h1>
+        </div>
+    )
+}
+
+export default Hero;
